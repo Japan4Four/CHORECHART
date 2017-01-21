f7fa020461d2027bf2037ac69f51871ff7c4e4dd
tupac <jaysicilia@gmail.com>
2017-01-21 15:46:40 -0800

    first attempt at views.py

--- tasks/views.py
+++ tasks/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the TASKS index.")
+
+def tast_list(request):
+    return HttpResponse (" This is the task list")
