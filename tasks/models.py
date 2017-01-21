89e399cf31c3c12a6d4cbe0eface624b387610f7
tupac <jaysicilia@gmail.com>
2017-01-21 14:19:47 -0800

    added task class to models.py

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task_text = models.CharField(max_length=200)
+    priority = models.IntegerField(default=0)
+    assignee = models.ForeignKey(Person, on_delete=models.CASCADE)
+    due_date = models.DateTimeField('date due')
+    complete_data = models.DateTimeField('date complete')
