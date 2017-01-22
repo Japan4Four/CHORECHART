4396ec9392fefe673f112d9d1f71847b7a92abde
tupac <jaysicilia@gmail.com>
2017-01-21 17:12:18 -0800

    edited views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 
+from .models import Task
 
 def index(request):
     return HttpResponse("Hello, world. You're at the TASKS index.")
 
 def task_list(request):
-    available_tasks = tasks.object.order_by('-priority')
+    available_tasks = Task.object.order_by('-priority')
     template = loader.get_template('tasks\index.html')
     context = {
         'available_tasks' : available_tasks,
